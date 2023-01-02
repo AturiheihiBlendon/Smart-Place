2fd6562359a91a40bc8978b2f3e3745b1ce0f95f
AturiheihiBlendon <2020blendon@gmail.com>
2023-01-02 18:09:43 +0300

    Django cors headers

--- Api/settings.py
+++ Api/settings.py
@@ -43,16 +43,23 @@ INSTALLED_APPS = [
     'knox',
     'Accounts',
     'Janitor.apps.JanitorConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 ROOT_URLCONF = 'Api.urls'
