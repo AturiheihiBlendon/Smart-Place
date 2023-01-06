8aecce880c58c9f2e6ea5b86ab6f2b3b68f3786d
AturiheihiBlendon <2020blendon@gmail.com>
2023-01-06 13:07:20 +0300

    Implement sms API on the frontend

--- smartplace/src/Pages/message.js
+++ smartplace/src/Pages/message.js
@@ -3,49 +3,28 @@ import { Link } from 'react-router-dom';
 import './message.css'
 
 
-const API = 'https://api.africastalking.com/version1/messaging?username=Mysite';
-// const username = 'Mysite';
+const API = 'http://127.0.0.1:8000/sms/create_sms/';
 
 function Send_message () {
 
-    const [to, setto] = useState('');
+    const [phone_number, setphone_number] = useState('');
     const [message, setmessage] = useState('');
-    const [username, setusername] = useState('Mysite');
 
-    const Send = async (refresh) => {
-        refresh.preventDefault();
-    
-        const request = new Request(
-          `${API}`,
-          {
-            body:JSON.stringify({ username, to, message }),
-            // body: new URLSearchParams({
-            //     'username': { username },
-            //     'to': { to },
-            //     'message': { message }
-            //   }),
-            headers: {
-               'Content-Type': 'application/x-www-form-urlencoded',
-            },method:'POST',
-            mode: "no-cors",
-          }
-          
-        );
-        const res = await fetch(request);
-        const data = await res.json();
-    
-        if (res.ok){
-          console.log(data);
-      }
-      else{
-          console.log("Failed Post method")
-      }
-        setto('');
-        setmessage('');
-        setusername('Mysite');
-      }
+        async function Send (refresh) {
+            refresh.preventDefault();
+            const formData = new FormData();
+            formData.append("phone_number",phone_number);
+            formData.append("message",message);
 
-
+            let res = await fetch(`${API}`,{
+                method: 'POST',
+                body:formData
+            });
+            if (res.ok){
+                alert("Message sent")
+            }
+            
+        }
 
     return (
         <div className="main">
@@ -70,12 +49,12 @@ function Send_message () {
                 <h3>Send Message</h3>
             </div>
             
-            <div className='content'>
-                <div className="message">
+            {/* <div className='content'>
+                <div className="form">
                     <div className="form-group">
                         <label htmlFor="title">Phone number</label>
-                        <input type="tel" name="to" id="to" 
-                        value={to} onChange={(e) =>{return setto(e.target.value)}}
+                        <input type="tel" name="phone_number" id="phone_number" 
+                        value={phone_number} onChange={(e) =>{return setphone_number(e.target.value)}}
                         className="form-control"/>
                     </div>
 
@@ -91,6 +70,32 @@ function Send_message () {
                         className="btn" />
                     </div>
                 </div>
+            </div> */}
+
+            <div className='content'>
+
+                <form action="">
+                    <div className="form">
+                        <div className="form-group">
+                            <label htmlFor="title">Phone number</label>
+                            <input type="tel" name="phone_number" id="phone_number" 
+                            value={phone_number} onChange={(e) =>{return setphone_number(e.target.value)}}
+                            className="form-control"/>
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="content">Text Message</label>
+                            <textarea name="content" id="" cols="30" rows="5" 
+                            value={message} onChange={(e) =>{return setmessage(e.target.value)}}
+                            className="form-control"></textarea>
+                        </div>
+
+                        <div className="form-group">
+                            <input type="submit" value="SEND" onClick={ Send }
+                            className="btn" />
+                        </div>
+                    </div>
+                </form>
             </div>
             
         </div>
